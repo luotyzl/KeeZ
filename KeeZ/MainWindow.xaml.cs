1603bf863402a8e9f3beb1249f98b4dcf953cc3b
RaymonL <luotyzl@gmail.com>
2025-06-30 22:53:30 +1200

    add system tray

--- KeeZ/MainWindow.xaml.cs
+++ KeeZ/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
 
 namespace KeeZ;
 
@@ -20,6 +23,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         InitializeWebView();
+        InitializeSystemTray();
     }
     
     private async void InitializeWebView()
@@ -32,5 +36,52 @@ public partial class MainWindow : Window
         {
             //ignore
         }
+    }
+    
+    private NotifyIcon _notifyIcon;
+    private void InitializeSystemTray()
+    {
+        _notifyIcon = new NotifyIcon
+        {
+            Icon = new Icon("favicon.ico"), // Your icon file
+            Text = "KeeZ",
+            Visible = true
+        };
+        
+        // Add context menu
+        var contextMenu = new ContextMenuStrip();
+        contextMenu.Items.Add("Show", null, (s, e) => ShowWindow());
+        contextMenu.Items.Add("Exit", null, (s, e) => ExitApplication());
+        _notifyIcon.ContextMenuStrip = contextMenu;
+        
+        // Double click to show window
+        _notifyIcon.DoubleClick += (s, e) => ShowWindow();
+        
+        // Handle window closing to minimize to tray instead
+        Closing += (s, e) =>
+        {
+            e.Cancel = true;
+            HideWindow();
+        };
+    }
+    private void HideWindow()
+    {
+        Hide();
+    }
+    private void ExitApplication()
+    {
+        _notifyIcon.Dispose();
+        Application.Current.Shutdown();
+    }
+    private void ShowWindow()
+    {
+        Show();
+        WindowState = WindowState.Normal;
+        Activate();
+    }
+    protected override void OnClosed(EventArgs e)
+    {
+        _notifyIcon?.Dispose();
+        base.OnClosed(e);
     }
 }
