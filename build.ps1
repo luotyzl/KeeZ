2dfbefb7d7b073f2fb42d4ebcfc0f8245d2abfa0
RaymonL <luotyzl@gmail.com>
2025-07-20 15:17:05 +1200

    remove deployment files

--- build.ps1
+++
@@ -1,3 +0,0 @@
-dotnet build ./src/Ursa/Ursa.csproj -c Release
-dotnet build ./src/Ursa.Themes.Semi/Ursa.Themes.Semi.csproj -c Release
-dotnet build ./src/Ursa.PrismExtension/Ursa.PrismExtension.csproj -c Release
