bea536b3d0595d6e84ba345d30e049237735c9f8
RaymonL <luotyzl@gmail.com>
2026-01-09 11:23:06 +1300

    Remove All WPF files

--- Semi.Avalonia.ColorPicker/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://irihi.tech/semi", "Semi.Avalonia.ColorPicker")]
--- Semi.Avalonia/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using Avalonia.Metadata;
-
-[assembly:XmlnsPrefix("https://irihi.tech/semi", "semi")]
-[assembly:XmlnsDefinition("https://irihi.tech/semi", "Semi.Avalonia")]
